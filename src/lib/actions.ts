14be001b9bcd53f61ad1fbde791fada6cefef10d
igbokwe-chibueze <igbokwechibueze@gmail.com>
2025-03-12 09:56:08 +0100

    Added authentication to actions.
    Signed-off-by: igbokwe-chibueze <igbokwechibueze@gmail.com>

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -2,8 +2,18 @@
 
 import { revalidatePath } from "next/cache";
 import prisma from "./db";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { redirect } from "next/navigation";
 
 export async function createPost(formData: FormData) {
+    // check if user is authenticated
+    const { isAuthenticated } = getKindeServerSession();
+    if (!isAuthenticated()) {
+        redirect("api/auth/login");
+    }
+
+    // normaly you would want to do a validation check here to make sure you are recieving a formData (e.g zod)
+    // if doing this, the fuction parameter would be "formData: unknown" and not "formData: FormData"
 
     const title = formData.get("title") as string;
     const body = formData.get("body") as string;
